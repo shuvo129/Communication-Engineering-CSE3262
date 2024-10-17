b2bef511975e56e6c623464be42cb62558968e68
MD Hasibul Khalid <105715738+shuvo129@users.noreply.github.com>
2024-10-17 12:46:15 +0600

    First Commit

---
+++ Communication lab/Communication lab/Frequency_modul.m
@@ -0,0 +1,27 @@
+Ac=input('Carrier signal Amplitude:');
+Am=input('Message signal Amplitude:');
+fc=input('Carrier signal Frequency:');
+fm=input('Message signal Frequency:');
+
+while fm>fc
+    disp('Message signal frequency is incorrect.');
+    fm=input('Message signal Frequency');
+end
+
+T=input('Time period:');
+t=0:0.001:T;
+
+Ym=Am*sin(2*pi*fm*t);
+subplot(3,1,1);
+plot(t,Ym);
+title('Message signal');
+
+Yc=Ac*sin(2*pi*fc*t);
+subplot(3,1,2);
+plot(t,Yc);
+title('Carrier signal');
+
+Z = Ac.*sin(2*pi*fc*t+((Am/Ac).*(2*pi*fm*t)));
+subplot(3, 1, 3);
+plot(t, Z, 'r');
+title('Modulated Signal');
---
+++ Communication lab/Communication lab/Unipolar.m
@@ -0,0 +1,44 @@
+bits=[1 0 1 0 1 1 0 0 1];
+bitduration=1;
+T=length(bits)*bitduration;
+fs=100;
+t=0:bitduration/fs:T;
+
+x=zeros(1,length(t));
+
+
+for i=1:length(bits)
+
+    if(bits(i)==1)
+
+      x((i-1)*fs+1:i*fs)=1;
+
+
+     else
+
+       x((i-1)*fs+1:i*fs)=0;
+
+
+    end
+
+
+
+end
+
+x(i*fs+1)=x(i*fs);
+
+
+plot(t,x,'Linewidth',3);
+
+counter=0;
+
+for i=1:fs:length(x)-1
+  counter=counter+1;
+    if(x(i)==1)
+      result(counter)=1;
+    else
+       result(counter)=0;
+    end
+end
+
+  disp('result')
